3370411fbfa966d593e30ce1e522098636a11f80
Prateek Bhatnagar <prateek89born@gmail.com>
2015-05-11 21:15:59 +0530

    reverting gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,18 +6,15 @@
 module.exports = function(grunt) {
 
     /* =====================================
-        Quest settings
+        Fevicol settings
     =======================================*/
-    var questSettings = grunt.file.readJSON("./quest_settings.json");
-
-    //tag folder name
-    var tagsFolder = questSettings.quest.tagFolder || "tags";
+    var fevicolSettings=grunt.file.readJSON("./fevicol.json");
+    var tagsFolder = fevicolSettings.fevicol.tagFolder||"tags";
 
     /* =====================================
         CSS Files List
     =======================================*/
-    var cssFilesList = grunt.file.readJSON("./css_files.json");
-    var compiledCssPath = "./css/quick_styles.css";
+    //var cssFilesList = grunt.file.readJSON("./code/css_filelist.json");
 
     /* =====================================
         JS Files List
@@ -30,74 +27,29 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON("package.json"),
 
         clean: {
-            pre_build: [tagsFolder + "/**/*.js", questSettings.quest.compiledStackFile, compiledCssPath],
-            post_build: [compiledCssPath.replace(".css", ".scss")]
+            pre_build: ["tags/**/*.js"]
+        },
+
+        riot: {
+            dist: {
+                expand: true,
+                cwd: tagsFolder,
+                src: "**/*.html",
+                dest: tagsFolder,
+                ext: ".js"
+            }
         },
 
         concat: {
             concat_tags: {
-                src: tagsFolder + "/**/*.html",
-                dest: questSettings.quest.compiledTagFile,
+                src: "tags/**/*.js",
+                dest: tagsFolder+"/compiled_app_tags.js",
             },
-            concat_js: {
-                src: [questSettings.quest.riotFilePath, questSettings.quest.questFilePath],
-                dest: questSettings.quest.compiledStackFile
-            },
-            concat_scss: {
-                src: cssFilesList.css_files,
-                dest: compiledCssPath.replace(".css", ".scss")
+            concat_app: {
+                src: ["riot_core/riot.js","tags/compiled_app_tags.js","riot_core/veronica.js"],
+                dest: "bin/app.js",
             }
         },
-
-        sass: {
-            transpile_scss: {
-                options: {
-                    compass: true,
-                    trace: true
-                },
-                files: [{
-                    expand: true,
-                    src: compiledCssPath.replace(".css", ".scss"),
-                    ext: '.css',
-                    dest: "./public/"
-                }]
-            }
-        },
-
-        appcache: {
-            app: {
-                dest: 'manifests/manifest.mf',
-                cache: {
-                    literals: [
-                        "/",
-                        "/public/tags/compiled_tags.html",
-                        "/public/javascripts/compiled_js.js",
-                        "/public/css/quick_styles.css",
-                        "/public/images/food.jpg",
-                        "/public/images/quick_logo.png"
-                    ]
-                },
-                network: '*'
-            }
-        },
-
-        watch: {
-            css: {
-                files: cssFilesList.css_files,
-                tasks: ['make-css'],
-                options: {
-                    nospawn: true
-                }
-            },
-            js: {
-                files: tagsFolder + "/**/*.html",
-                tasks: ['make-js'],
-                options: {
-                    nospawn: true
-                }
-            }
-
-        }
 
     });
 
@@ -110,21 +62,7 @@ module.exports = function(grunt) {
     // Load the plugin that provides the "concat" task.
     grunt.loadNpmTasks("grunt-contrib-concat");
 
-    // Load the plugin that provides the "sass" task.
-    grunt.loadNpmTasks('grunt-contrib-sass');
-
-    // Load the grunt watch so that every time grunt compiles automatically
-    grunt.loadNpmTasks('grunt-contrib-watch');
-
-    // Load the grunt app chache so that app cache is updated automatically
-    grunt.loadNpmTasks('grunt-appcache');
-
-    //Task for building css static contents of the application
-    grunt.registerTask("make-css", ["concat:concat_scss", "sass:transpile_scss"]);
-
-    //Task for building javascript static contents of the application
-    grunt.registerTask("make-js", ["concat:concat_tags", "concat:concat_js"]);
 
     //Task for building the static contents of the application
-    grunt.registerTask("default", ["clean:pre_build", "make-css", "make-js", "appcache:app", "clean:post_build"]);
+    grunt.registerTask("default", ["clean:pre_build","riot", "concat:concat_tags","concat:concat_app"]);
 };
